f832b411adeb27360442d73940cdc1c1c1ff7383
Roy Mosby <ego.madking@gmail.com>
2022-10-15 22:45:34 -0400

    use component instead of children

--- src/features/slotMachineComponents/reelRow.js
+++ src/features/slotMachineComponents/reelRow.js
@@ -1,10 +1,14 @@
 import { React } from "react";
+import styles from "./reelRowStyles.module.css";
+import { SymbolsRow } from "./symbolsRow";
+import { MenuItemRow } from "./menuItemRow";
 
-export function ReelRow({ children }) {
+export function ReelRow({ day }) {
   return (
-    <div>
-      <h3>Reel Row</h3>
-      {children}
+    <div className={styles.wrapper}>
+      <span>{day}: </span>
+      <SymbolsRow />
+      <button role="button">Hold</button>
     </div>
   );
 }
