87949a1508672b6b545db879e1410ac19c54f511
Roy Mosby <ego.madking@gmail.com>
2022-10-22 10:42:38 -0400

    update header

--- src/common/header.js
+++ src/common/header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./header.module.css";
 
 export function Header() {
   return (
-    <header>
-      <h3>This is a header</h3>
+    <header className={styles.header}>
+      <h1>MENU JACKPOT</h1>
+      <h2>Spin your way to a meal plan!</h2>
     </header>
   );
 }
