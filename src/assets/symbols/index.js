047a94d75570ac3d768dc0427cb78f599c573d09
Roy Mosby <ego.madking@gmail.com>
2022-10-23 11:42:17 -0400

    create barrel file

---
+++ src/assets/symbols/index.js
@@ -0,0 +1,17 @@
+import apple from "./apple.svg";
+import bananna from "./bananna.svg";
+import cherry from "./cherry.svg";
+import grape from "./grape.svg";
+import pear from "./pear.svg";
+import raspberry from "./raspberry.svg";
+import watermelon from "./watermelon.svg";
+
+export const symbols = {
+  apple,
+  bananna,
+  cherry,
+  grape,
+  pear,
+  raspberry,
+  watermelon,
+};
