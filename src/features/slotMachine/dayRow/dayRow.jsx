f4906cabae26d50f84c7ccebcf3268b0f48fe4a0
Roy Mosby <ego.madking@gmail.com>
2022-11-15 12:09:58 -0500

    changes day button styles when held

--- src/features/slotMachine/dayRow/dayRow.jsx
+++ src/features/slotMachine/dayRow/dayRow.jsx
@@ -1,13 +1,21 @@
-import { React } from "react";
+import { React, useState } from "react";
 import styles from "./dayRowStyles.module.css";
 import { Reel } from "./reel/reel";
 
 export function DayRow({ day, isPressed }) {
+  const [isHeld, setIsHeld] = useState(false);
+
   return (
     <div className={styles.wrapper}>
       <span>{day}</span>
-      <Reel isPressed={isPressed} />
-      <button role="button">Hold</button>
+      <Reel isPressed={isPressed} isHeld={isHeld} />
+      <button
+        role="button"
+        onClick={() => setIsHeld(!isHeld)}
+        className={isHeld ? styles.isHeld : ""}
+      >
+        {isHeld ? "Release" : "Hold"}
+      </button>
     </div>
   );
 }
