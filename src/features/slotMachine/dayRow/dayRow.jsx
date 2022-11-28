37f84b6534b50f599e1f6e1be1d99ff462913c57
Roy Mosby <ego.madking@gmail.com>
2022-11-28 08:17:47 -0500

    update interaction logic

--- src/features/slotMachine/dayRow/dayRow.jsx
+++ src/features/slotMachine/dayRow/dayRow.jsx
@@ -1,20 +1,53 @@
+import { useEffect } from "react";
 import { React, useState } from "react";
 import styles from "./dayRowStyles.module.css";
 import { Reel } from "./reel/reel";
 
-export function DayRow({ day, isPressed }) {
-  const [isHeld, setIsHeld] = useState(false);
+export function DayRow({
+  day,
+  spinIsPressed,
+  spriteSheetConfig,
+  isSpinning,
+  mealList,
+  firstSpin,
+}) {
+  const [rowIsHeld, setRowIsHeld] = useState(false);
+  const [buttonText, setButtonText] = useState("Press spin");
+  const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (firstSpin) {
+      return; //noop
+    } else if (rowIsHeld) {
+      setButtonText("Release");
+    } else {
+      setButtonText("Hold");
+    }
+  }, [firstSpin, rowIsHeld]);
+
+  useEffect(() => {
+    if (firstSpin || isSpinning) {
+      setButtonIsDisabled(true);
+    } else {
+      setButtonIsDisabled(false);
+    }
+  }, [firstSpin, isSpinning]);
 
   return (
     <div className={styles.wrapper}>
       <span>{day}</span>
-      <Reel isPressed={isPressed} isHeld={isHeld} />
+      <Reel
+        spinIsPressed={spinIsPressed}
+        rowIsHeld={rowIsHeld}
+        spriteSheetConfig={spriteSheetConfig}
+        mealList={mealList}
+      />
       <button
-        role="button"
-        onClick={() => setIsHeld(!isHeld)}
-        className={isHeld ? styles.isHeld : ""}
+        disabled={buttonIsDisabled}
+        onClick={() => setRowIsHeld(!rowIsHeld)}
+        className={rowIsHeld ? styles.isHeld : ""}
       >
-        {isHeld ? "Release" : "Hold"}
+        {buttonText}
       </button>
     </div>
   );
