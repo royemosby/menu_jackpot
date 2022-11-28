a13772c675476c8adc7eae5876fc92ec06833878
Roy Mosby <ego.madking@gmail.com>
2022-11-28 08:15:07 -0500

    add state for configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import { useState } from "react";
+
+import "./App.css";
+import { Admin } from "./features/admin/admin";
+import { Footer } from "./common/footer/footer";
+import { Header } from "./common/header/header";
+import { mealList as mealListArray } from "./assets/mealList";
 import { SlotMachine } from "./features/slotMachine/slotMachine";
-import { AdminIndex } from "./features/admin/index";
-import { Footer } from "./common/footer";
-import { Header } from "./common/header";
-import { mealList } from "./assets/mealList";
+import { Nav } from "./common/nav/nav";
 
 function App() {
+  const [spriteSheetConfig, setSpriteSheetConfig] = useState({
+    columns: 6,
+    rowsMultiplier: 10,
+  });
+
+  const [mealList, setMealList] = useState([...mealListArray]);
+
   return (
     <Router>
+      <Nav />
       <Header />
       <Routes>
-        <Route path="/" element={<SlotMachine mealList={mealList} />}></Route>
+        <Route
+          path="/"
+          element={
+            <SlotMachine
+              mealList={mealList}
+              spriteSheetConfig={spriteSheetConfig}
+            />
+          }
+        ></Route>
         <Route
           path="/admin"
-          element={<AdminIndex mealList={mealList} />}
+          element={
+            <Admin
+              mealList={mealList}
+              setMealList={setMealList}
+              spriteSheetConfig={spriteSheetConfig}
+              setSpriteSheetConfig={setSpriteSheetConfig}
+            />
+          }
         ></Route>
         <Route path="*" element={<Navigate to={{ pathname: "/" }} />} />
       </Routes>
