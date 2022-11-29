f70044771a7cb0cedaa45b38e1d6d6bd7fcc47a9
Roy Mosby <ego.madking@gmail.com>
2022-11-29 08:39:18 -0500

    remove router and admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,23 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import { useState } from "react";
-
 import "./App.css";
-import { Admin } from "./features/admin/admin";
+
 import { Footer } from "./common/footer/footer";
 import { Header } from "./common/header/header";
-import { mealList as mealListArray } from "./assets/mealList";
+import { mealList } from "./assets/mealList";
 import { SlotMachine } from "./features/slotMachine/slotMachine";
-import { Nav } from "./common/nav/nav";
 
 function App() {
-  const [spriteSheetConfig, setSpriteSheetConfig] = useState({
+  const spriteSheetConfig = {
     columns: 6,
     rowsMultiplier: 10,
-  });
-
-  const [mealList, setMealList] = useState([...mealListArray]);
+  };
 
   return (
-    <Router>
-      <Nav />
+    <>
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <SlotMachine
-              mealList={mealList}
-              spriteSheetConfig={spriteSheetConfig}
-            />
-          }
-        ></Route>
-        <Route
-          path="/admin"
-          element={
-            <Admin
-              mealList={mealList}
-              setMealList={setMealList}
-              spriteSheetConfig={spriteSheetConfig}
-              setSpriteSheetConfig={setSpriteSheetConfig}
-            />
-          }
-        ></Route>
-        <Route path="*" element={<Navigate to={{ pathname: "/" }} />} />
-      </Routes>
+      <SlotMachine mealList={mealList} spriteSheetConfig={spriteSheetConfig} />
+
       <Footer />
-    </Router>
+    </>
   );
 }
 
